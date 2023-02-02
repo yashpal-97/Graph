b4a36be450d8121f5751d7f2b4348ba8bc13b896
YASHPAL KATIYAR <109839369+yashpal-97@users.noreply.github.com>
2023-02-02 10:16:57 +0530

    Create Number Of Enclaves.cpp

---
+++ Number Of Enclaves.cpp
@@ -0,0 +1,54 @@
+You are given an n x m binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+
+A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+
+Find the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves
+
+
+
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0||i==n-1||j==0||j==m-1){
+                    if(grid[i][j]==1){
+                    vis[i][j]=1;
+                    q.push({i,j});
+                    }
+
+                }
+            }
+        }
+
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]&&grid[nrow][ncol]!=0){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1&&vis[i][j]==0){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+
+    }
+};
